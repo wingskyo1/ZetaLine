752f644de650b83ffdb0a0efc1a18477357277ba
Wing <wingskyo1@gmail.com>
2018-12-07 16:59:19 +0800

    no message

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ var bot = linebot({
 
   var timer;
   var regionData = [];
-  var test22=[];
   _getJSON();
   
   _bot();
@@ -43,7 +42,7 @@ var bot = linebot({
             if (msg.indexOf(e.SiteName) != -1) {
                 replyMsg =e.County + e.SiteName + '\n PM2.5 數值為 ' + e.pm + '\n 空氣品質 AQI 為 '+e.AQI;
             }
-            else if (_hasCountry(msg)!==''){
+            else if (msg.indexOf(e.County) != -1){
                 replyMsg +=e.County +" 設有檢測站的區域有 \n "
                 const result = regionData.filter(data => data.County === e.County);
                 if(result.length !==0){
@@ -51,10 +50,11 @@ var bot = linebot({
                         replyMsg += data + ", ";
                     });
                 }
+                replyMsg += " \n ヽ( ° ▽°)ノ";
             }
           });
           if (replyMsg == '') {
-            replyMsg = '你所輸入的區可能沒有空氣監測,你想問的地方在什麼城市呢 ? ヽ( ° ▽°)ノ ';
+            replyMsg = '輸入的區域可能沒有空氣監測 ^_^|||, 是在什麼城市呢 ? (^ρ^)/ ';
           }
         }
             return '';
@@ -65,32 +65,14 @@ var bot = linebot({
         }).catch(function(error) {
           console.log('error');
         });
-    
 }
 
-function _hasCountry(msg){
-
-    County.forEach(function(element){
-        if(msg.indexOf(element)){
-            return element;
-        }
-        else return '';
-    });
-
-
-}
 
 
 function _getJSON() {
     clearTimeout(timer);
     getJSON('http://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000259/?format=json&sort=County', function(error, response) {
-        //console.log("json 權應啦  = "+JSON.stringify(response, null, 7));
 
-
-        var data = JSON.stringify(response.
-            result.records, null, 7);
-       // console.log(response.result.records);
-        //console.log("回應內容 "+data);
 
         response.result.records.forEach(function(e, i) {
          regionData[i] = [];
@@ -99,11 +81,10 @@ function _getJSON() {
          regionData[i].AQI = e.AQI;
          regionData[i].pm = e['PM2.5'] * 1;
          regionData[i].PM10 = e.PM10 * 1;
-         //test22[i] = [];
 
        });
-       const distinctCountry = [...new Set(regionData.map(x=>x.County))]
-       console.log(distinctCountry);
+       //const distinctCountry = [...new Set(regionData.map(x=>x.County))]
+       //console.log(distinctCountry);
     });
     timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
   }
