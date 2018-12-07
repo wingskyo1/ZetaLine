3f4993ad6afdd01405b9a80334fa94b654477b1d
Wing <wingskyo1@gmail.com>
2018-12-07 14:06:10 +0800

    forget import

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
 var linebot = require('linebot');
 var express = require('express');
+var getJSON = require('get-json');
+
+var bot = linebot({
+    channelId: '1628760097',
+    channelSecret: 'fa6ad7e42c864158294e81fdea65932e',
+    channelAccessToken: 'vfUeCL+a4I9SUwH24sfN1Ak9spTN/dRaAO6Ix1LsSMmMQsweU3Z/FNLzg/rUmRfYep5tjKw6ZK1OzgOW3kcMZgZplJLpIRmr9bxH2gDzfUvUfKf3oCO2vgq9K/jcLiGUZdlqHvjxn96lqBh57B1uegdB04t89/1O/w1cDnyilFU='
+  });
 
 var timer;
 var pm = [];
 _getJSON();
 
-
-var bot = linebot({
-  channelId: '1628760097',
-  channelSecret: 'fa6ad7e42c864158294e81fdea65932e',
-  channelAccessToken: 'vfUeCL+a4I9SUwH24sfN1Ak9spTN/dRaAO6Ix1LsSMmMQsweU3Z/FNLzg/rUmRfYep5tjKw6ZK1OzgOW3kcMZgZplJLpIRmr9bxH2gDzfUvUfKf3oCO2vgq9K/jcLiGUZdlqHvjxn96lqBh57B1uegdB04t89/1O/w1cDnyilFU='
-});
 
 //這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
 bot.on('message', function(event) {
@@ -29,7 +30,7 @@ bot.on('message', function(event) {
   }
 });
 
-function npmfilter(message){
+function npmfilter(event){
 
         if (event.message.type == 'text') {
             var msg = event.message.text;
