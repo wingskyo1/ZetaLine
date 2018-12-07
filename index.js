a0c1d004975a1a1bd8c36024f802ad1b7e923488
Wing <wingskyo1@gmail.com>
2018-12-07 13:38:35 +0800

    test

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
-var server = app.listen( 8080, function() {
+var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log('目前的port是', port);
 });
