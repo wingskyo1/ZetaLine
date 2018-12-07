ba2c497de6b7cc1cb6d72b999e9bcbd1e3b786d6
Wing <wingskyo1@gmail.com>
2018-12-07 13:43:55 +0800

    success

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ bot.on('message', function(event) {
   if (event.message.type = 'text') {
     var msg = event.message.text;
   //收到文字訊息時，直接把收到的訊息傳回去
-    event.reply(msg).then(function(data) {
-      // 傳送訊息成功時，可在此寫程式碼 
-      console.log(msg);
-    }).catch(function(error) {
-      // 傳送訊息失敗時，可在此寫程式碼 
-      console.log('錯誤產生，錯誤碼：'+error);
-    });
+    // event.reply(msg).then(function(data) {
+    //   // 傳送訊息成功時，可在此寫程式碼 
+    //   console.log(msg);
+    // }).catch(function(error) {
+    //   // 傳送訊息失敗時，可在此寫程式碼 
+    //   console.log('錯誤產生，錯誤碼：'+error);
+    // });
   }
 });
 
