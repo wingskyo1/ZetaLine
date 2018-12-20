218ba952180e30ba9b901669f15745371c31b53d
Wing <wingskyo1@gmail.com>
2018-12-20 17:57:06 +0800

    add limit to ask

--- Modules/whatToEat.js
+++ Modules/whatToEat.js
@@ -1,7 +1,7 @@
 var getJSON = require('get-json');
 
-var foodList = ["肯德基","喬品","鮮肉湯包","阿蓮和粉","名都飲食","南豐滷肉飯","凌波"];
-
+var foodList = ["肯德基", "喬品", "鮮肉湯包", "阿蓮和粉", "名都飲食", "南豐滷肉飯", "凌波"];
+var lasttime;
 
 module.exports = {
     getFood: function (event) {
@@ -9,16 +9,21 @@ module.exports = {
             var msg = event.message.text;
             var replyMsg = '';
             if (msg.match(/(吃什麼)/)) {
-                replyMsg = this.getRandomFood();
-                status = 1;
+                var date = new date();
+                if (date.gatTime() - lasttime < 5 * 60 * 1000) {
+                    replyMsg = "你不能一直問哦~~~!";
+                } else {
+                    replyMsg = this.getRandomFood();
+                    lasttime = date.getTime();
+                }
             }
-                return replyMsg;
+            return replyMsg;
         }
     },
 
     getRandomFood: function () {
-        var randomNum = Math.floor(Math.random()*(foodList.length+1));
-        
+        var randomNum = Math.floor(Math.random() * (foodList.length + 1));
+
         return foodList[randomNum];
     },
 
