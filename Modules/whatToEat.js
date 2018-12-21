fc4ec8d1c3d04893106720507024afa4a8a7f2cb
Wing <wingskyo1@gmail.com>
2018-12-21 11:41:44 +0800

    吃什麼 改成認人 並顯示剩餘時間

--- Modules/whatToEat.js
+++ Modules/whatToEat.js
@@ -2,22 +2,25 @@ var getJSON = require('get-json');
 
 var foodList = ["肯德基", "喬品", "鮮肉湯包", "阿蓮和粉", "名都飲食", "南豐滷肉飯", "凌波"];
 var lasttime=0;
-
+var userlist={};
 module.exports = {
     getFood: function (event) {
         if (event.message.type == 'text') {
             var msg = event.message.text;
+            var userID = event.source.userId;
             var replyMsg = '';
             if (msg.match(/(吃什麼)/)) {
                 var date = new Date();
-                var difference = date.getTime()-lasttime;
-                if (difference < 5 * 60 * 1000) {
+                var difference = (date.getTime()-userlist[userID])/1000;
+                var needToWait = 300- difference;
+
+                if (needToWait > 0) {
                     console.log("test : " + date.getTime())
                     console.log("lasttime : "+ lasttime)
-                    replyMsg = "你不能一直問哦~~~! 每五分鐘才能問一次";
+                    replyMsg = "你不能賴皮哦~~~! 請再等 " + Math.round(needToWait) + " 秒後發問";
                 } else {
                     replyMsg = this.getRandomFood();
-                    lasttime = date.getTime();
+                    userlist[userID] = date.getTime();
                 }
             }
             return replyMsg;
--- index.js
+++ index.js
@@ -10,6 +10,12 @@ var bot = linebot({
     channelAccessToken: 'vfUeCL+a4I9SUwH24sfN1Ak9spTN/dRaAO6Ix1LsSMmMQsweU3Z/FNLzg/rUmRfYep5tjKw6ZK1OzgOW3kcMZgZplJLpIRmr9bxH2gDzfUvUfKf3oCO2vgq9K/jcLiGUZdlqHvjxn96lqBh57B1uegdB04t89/1O/w1cDnyilFU='
 });
 
+// var bot = linebot({
+// channelId: '1632921102',
+// channelSecret: '3ad4d8030b92afe9fa1359ca4b172cc3',
+// channelAccessToken: 'Gp4vfmzBuI1lKntC1VS89OaL+eC6Z+d1KamkwsiyhY+2zAc0yDgaebSPukQFPKoi1kdoLNSt/zAztShxGUfl4LB8TI1GU8yF/BX2p0SP+kVKry6z5O9t9X4CfKVi4LiYRxlA2vqlPHV3aY6AogGT1wdB04t89/1O/w1cDnyilFU='
+// });
+
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
@@ -36,7 +42,7 @@ function _botStart() {
         
         var foodMsg = whatToEat.getFood(event) ;
 
-        sendMsg = (foodMsg == "") ? sendMsg : foodMsg;
+        sendMsg = foodMsg  || sendMsg;
 
         //如果有訊息送出
         if (sendMsg !== undefined) {
