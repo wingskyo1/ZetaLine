08c522e6a8a04be28db7d3783c05c5924c33537a
Wing <wingskyo1@gmail.com>
2018-12-24 14:06:19 +0800

    Set variable to env

--- config.js
+++ config.js
@@ -3,15 +3,11 @@ var express = require('express');
 
 module.exports = {
     bot: ({
-        channelId: '1628760097',
-        channelSecret: 'fa6ad7e42c864158294e81fdea65932e',
-        channelAccessToken: 'vfUeCL+a4I9SUwH24sfN1Ak9spTN/dRaAO6Ix1LsSMmMQsweU3Z/FNLzg/rUmRfYep5tjKw6ZK1OzgOW3kcMZgZplJLpIRmr9bxH2gDzfUvUfKf3oCO2vgq9K/jcLiGUZdlqHvjxn96lqBh57B1uegdB04t89/1O/w1cDnyilFU='
+        channelId: process.env.channelId,
+        channelSecret: process.env.channelSecret,
+        channelAccessToken: process.env.channelAccessToken
     }),
-    testbot: ({
-        channelId: '1632921102',
-        channelSecret: '3ad4d8030b92afe9fa1359ca4b172cc3',
-        channelAccessToken: 'Gp4vfmzBuI1lKntC1VS89OaL+eC6Z+d1KamkwsiyhY+2zAc0yDgaebSPukQFPKoi1kdoLNSt/zAztShxGUfl4LB8TI1GU8yF/BX2p0SP+kVKry6z5O9t9X4CfKVi4LiYRxlA2vqlPHV3aY6AogGT1wdB04t89/1O/w1cDnyilFU='
-    }),
+
 
     // location是中心點經緯度
     // radius是搜尋範圍
